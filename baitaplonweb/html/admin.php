93a977439b79fa33b77823b4aab09d93265ff09d
nguyenpatz <94157413+nguyenpatz@users.noreply.github.com>
2021-12-08 15:54:38 +0700

    Add files via upload

---
+++ baitaplonweb/html/admin.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ADMIN</title>
+</head>
+<body>
+    <header>
+        <img src="" alt="">
+        <nav class="nav">
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>                    
+            </ul>
+        </nav>
+    </header>
+    <div class="container"></div>
+    <footer></footer>
+</body>
+</html>
